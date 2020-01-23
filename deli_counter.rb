216b7f09923e1ba203c689bc439684ac7a248977
Dan Foley <dan@dan-foley.com>
2020-01-23 23:59:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,30 @@ require "pry"
 
 # Write your code here.
 
+line = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
 def line(arr)
-  # binding.pry
   if arr.size == 0
     puts "The line is currently empty."
   else
-    intro = "The line is currently: "
-    # binding.pry
+    new_arr = []
+    intro = "The line is currently:"
+    arr.each_with_index {|name, index| new_arr << "#{index + 1}. #{name}"}
+    puts new_arr.unshift(intro).join(" ")
   end
 end
+
+def take_a_number(line, new_cust)
+  line.push(new_cust)
+  puts "Welcome, #{new_cust}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+  if line.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line.shift}."
+  end
+end
+
+# line(line)
