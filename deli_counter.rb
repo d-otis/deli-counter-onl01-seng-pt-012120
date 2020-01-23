42323c0104d725f9b6d32b54f05ceb7b6a7686a6
Dan Foley <dan@dan-foley.com>
2020-01-23 23:31:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
+require "pry"
+
 # Write your code here.
+
+def line(arr)
+  # binding.pry
+  if arr.size == 0
+    puts "The line is currently empty."
+  else
+    intro = "The line is currently: "
+    # binding.pry
+  end
+end
